b9fc82fc94538aee148bfc1aceddbcdd1285b7e8
DariusPirvulescu <organicdarius@gmail.com>
2020-07-01 19:37:05 +0300

    added flatpickr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,11 +24,23 @@ require("channels")
 
 // External imports
 import "bootstrap";
-
+import flatpickr from "flatpickr";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = currentDate.getMonth() + 1;
+  const day = currentDate.getDate();
+
+  flatpickr(".date-picker", {
+     altInput: true,
+    altFormat: "F j, Y",
+    dateFormat: "Y-m-d"
+  });
+
 });
