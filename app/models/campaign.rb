8659fc7d4bc5b7f2966382f4bddc2726dc6a9d3a
carlos madeira <cfmadeira@gmail.com>
2020-07-02 12:19:53 +0100

    .env added

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,4 +1,18 @@
 class Campaign < ApplicationRecord
   belongs_to :project
   has_many :shareholders
+
+  validates :name, presence: true, uniqueness: true
+
+  with_options if: :active? do
+    validates :start_date, presence: true
+    validates :end_date, presence: true
+    validates :description, presence: true
+    validates :amount, presence: true
+    validates :percentage, presence: true
+  end
+
+  def active?
+    status
+  end
 end
