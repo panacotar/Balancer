9f940d1672181ce5c6b1ceb926f43f48d9b8efb8
DariusPirvulescu <organicdarius@gmail.com>
2020-07-05 16:20:01 +0100

    refactored and added balancer colors to progress bar

--- app/javascript/components/progress_bar.js
+++ app/javascript/components/progress_bar.js
@@ -1,13 +1,13 @@
 export const showProgress = () => {
-    // Progress Bar Variables
-    const progressContainer = document.querySelectorAll(".js-progress-container");
-    const progressContainerWidth = progressContainer.offsetWidth;
+  // Progress Bar Variables
+  const progressContainer = document.querySelectorAll(".js-progress-container");
+  const progressContainerWidth = progressContainer.offsetWidth;
 
-    const progressDivs = document.querySelectorAll(".progress")
-
+  const progressDivs = document.querySelectorAll(".progress")
 
   progressDivs.forEach(progressDiv => {
-    //// Access to campaigns!!
+
+    //// Have Access to campaigns!!
 
     // console.log(progressDiv.dataset.campaigns)
     // console.log("progressDiv", progressDiv)
@@ -16,16 +16,17 @@ export const showProgress = () => {
     let progressWidth = progress.offsetWidth;
 
     // here you can set the actual progress (progressWidth)!!
-      // progressWidth = 50
-      progressWidth = progressDiv.dataset.campaigns.length + 25
+    // in percentage. how fucking awesome!
+    // progressWidth = 50
+    progressWidth = progressDiv.dataset.campaigns.length + 25
 
-      // different colors for progress!!
-      if(progressWidth >= 25){
-          progress.classList.add("tracker-progress-green");
-      } else {
-          progress.classList.remove("tracker-progress-green");
-      }
-      progress.style.width = progressWidth + "%";
+    // different colors for progress!!
+    if(progressWidth >= 50){
+        progress.classList.add("tracker-progress-green");
+    } else {
+        progress.classList.remove("tracker-progress-green");
+    }
+    progress.style.width = progressWidth + "%";
   })
 }
 
