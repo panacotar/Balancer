52142bd8659cf706ade5556a89bbe92181923307
Amanda <amanda@ciobanu.org>
2020-07-08 21:24:11 +0100

    fix the chart

--- app/javascript/components/chart.js
+++ app/javascript/components/chart.js
@@ -3,95 +3,98 @@ import * as am4charts from "@amcharts/amcharts4/charts";
 import am4themes_spiritedaway from "@amcharts/amcharts4/themes/spiritedaway";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 
-/* Chart code */
-// Themes begin
-am4core.useTheme(am4themes_spiritedaway);
-am4core.useTheme(am4themes_animated);
-// Themes end
+const makeChart = (chartdiv) => {
+  /* Chart code */
+  // Themes begin
+  am4core.useTheme(am4themes_spiritedaway);
+  am4core.useTheme(am4themes_animated);
+  // Themes end
 
+  let chart = am4core.create(chartdiv, am4charts.XYChart);
 
-let chart = am4core.create("chartdiv", am4charts.XYChart);
+  chart.data = [{
+  "country": "Portugal",
+  "visits": 2025
+  }, {
+  "country": "Ireland",
+  "visits": 1882
+  }, {
+  "country": "Spain",
+  "visits": 1809
+  }, {
+  "country": "Germany",
+  "visits": 1322
+  }, {
+  "country": "Croatia",
+  "visits": 1122
+  }, {
+  "country": "France",
+  "visits": 1114
+  }, {
+  "country": "Chile",
+  "visits": 984
+  }, {
+  "country": "Uruguai",
+  "visits": 711
+  }, {
+  "country": "Greece",
+  "visits": 665
+  }, {
+  "country": "USA",
+  "visits": 580
+  }, {
+  "country": "UK",
+  "visits": 443
+  }, {
+  "country": "Canada",
+  "visits": 441
+  }];
 
-chart.data = [{
- "country": "Portugal",
- "visits": 2025
-}, {
- "country": "Ireland",
- "visits": 1882
-}, {
- "country": "Spain",
- "visits": 1809
-}, {
- "country": "Germany",
- "visits": 1322
-}, {
- "country": "Croatia",
- "visits": 1122
-}, {
- "country": "France",
- "visits": 1114
-}, {
- "country": "Chile",
- "visits": 984
-}, {
- "country": "Uruguai",
- "visits": 711
-}, {
- "country": "Greece",
- "visits": 665
-}, {
- "country": "USA",
- "visits": 580
-}, {
- "country": "UK",
- "visits": 443
-}, {
- "country": "Canada",
- "visits": 441
-}];
+  chart.padding(40, 40, 40, 40);
 
-chart.padding(40, 40, 40, 40);
+  let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+  categoryAxis.renderer.grid.template.location = 0;
+  categoryAxis.dataFields.category = "country";
+  categoryAxis.renderer.minGridDistance = 60;
+  categoryAxis.renderer.inversed = true;
+  categoryAxis.renderer.grid.template.disabled = true;
 
-let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
-categoryAxis.renderer.grid.template.location = 0;
-categoryAxis.dataFields.category = "country";
-categoryAxis.renderer.minGridDistance = 60;
-categoryAxis.renderer.inversed = true;
-categoryAxis.renderer.grid.template.disabled = true;
+  let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+  valueAxis.min = 0;
+  valueAxis.extraMax = 0.1;
+  //valueAxis.rangeChangeEasing = am4core.ease.linear;
+  //valueAxis.rangeChangeDuration = 1500;
 
-let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-valueAxis.min = 0;
-valueAxis.extraMax = 0.1;
-//valueAxis.rangeChangeEasing = am4core.ease.linear;
-//valueAxis.rangeChangeDuration = 1500;
+  let series = chart.series.push(new am4charts.ColumnSeries());
+  series.dataFields.categoryX = "country";
+  series.dataFields.valueY = "visits";
+  series.tooltipText = "{valueY.value}"
+  series.columns.template.strokeOpacity = 0;
+  series.columns.template.column.cornerRadiusTopRight = 10;
+  series.columns.template.column.cornerRadiusTopLeft = 10;
+  //series.interpolationDuration = 1500;
+  //series.interpolationEasing = am4core.ease.linear;
+  let labelBullet = series.bullets.push(new am4charts.LabelBullet());
+  labelBullet.label.verticalCenter = "bottom";
+  labelBullet.label.dy = -10;
+  labelBullet.label.text = "{values.valueY.workingValue.formatNumber('#.')}";
 
-let series = chart.series.push(new am4charts.ColumnSeries());
-series.dataFields.categoryX = "country";
-series.dataFields.valueY = "visits";
-series.tooltipText = "{valueY.value}"
-series.columns.template.strokeOpacity = 0;
-series.columns.template.column.cornerRadiusTopRight = 10;
-series.columns.template.column.cornerRadiusTopLeft = 10;
-//series.interpolationDuration = 1500;
-//series.interpolationEasing = am4core.ease.linear;
-let labelBullet = series.bullets.push(new am4charts.LabelBullet());
-labelBullet.label.verticalCenter = "bottom";
-labelBullet.label.dy = -10;
-labelBullet.label.text = "{values.valueY.workingValue.formatNumber('#.')}";
+  chart.zoomOutButton.disabled = true;
 
-chart.zoomOutButton.disabled = true;
+  // as by default columns of the same series are of the same color, we add adapter which takes colors from chart.colors color set
+  series.columns.template.adapter.add("fill", function (fill, target) {
+  return chart.colors.getIndex(target.dataItem.index);
+  });
 
-// as by default columns of the same series are of the same color, we add adapter which takes colors from chart.colors color set
-series.columns.template.adapter.add("fill", function (fill, target) {
- return chart.colors.getIndex(target.dataItem.index);
-});
+  setInterval(function () {
+  am4core.array.each(chart.data, function (item) {
+    item.visits += Math.round(Math.random() * 200 - 100);
+    item.visits = Math.abs(item.visits);
+  })
+  chart.invalidateRawData();
+  }, 3000)
 
-setInterval(function () {
- am4core.array.each(chart.data, function (item) {
-   item.visits += Math.round(Math.random() * 200 - 100);
-   item.visits = Math.abs(item.visits);
- })
- chart.invalidateRawData();
-}, 3000)
+  categoryAxis.sortBySeries = series;
+}
 
-categoryAxis.sortBySeries = series;
+export { makeChart }
