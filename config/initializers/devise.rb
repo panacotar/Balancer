916cf95e26dd36372777dec497a52855174cf010
Darius Pirvulescu <organicdarius@gmail.com>
2023-10-03 12:33:26 +0100

    Change devise mailer_sender config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'Balancer Team <support@balancer.com>'
+  config.mailer_sender = 'Balancer Team <securedogapp+balancer@gmail.com>'
 
   # Configure the class responsible to send e-mails.
   config.mailer = 'Devise::Mailer'
